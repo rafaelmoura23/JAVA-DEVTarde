7648c82849828ebacfdcef555763ffe1b80278bc
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-04-18 16:54:15 -0300

    18.04

--- IteracaoFor/ExerciciosFor.java
+++ IteracaoFor/ExerciciosFor.java
@@ -127,6 +127,15 @@ public class ExerciciosFor {
             }
         }
 
+        public void exercicio5extra() {    //Gambiarra
+            int numeros[] = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+            int nPar[]= new int[numeros.length];  
+            int nImpar[]= new int[numeros.length]; 
+            
+            }
+
+            
+
 
 
 
