76a881d6d4c08ba96dc5ccf9ae46fec7b3afa616
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-04-21 23:36:28 +0000

    21.04

--- IteracaoFor/ExerciciosExtras.java
+++ IteracaoFor/ExerciciosExtras.java
@@ -8,6 +8,49 @@ public class ExerciciosExtras {
     public void exercicio1() {
         int vetorNumeros[] = new int[1];
         
+    }
+
+    public void exercicio2() {
+        
+    }
+
+    public void exercicio3() {
+        
+    }
+
+    public void exercicio4() {
+        
+    }
+
+    public void exercicio5() {
+        
+    }
+
+    public void exercicio6() {
+        
+    }
+
+    public void exercicio7() {
+        
+    }
+
+    public void exercicio8() {
+        
+    }
+
+    public void exercicio9() {
+        
+    }
+
+    public void exercicio10() {
+        
+    }
+
+    public void exercicio11() {
+        
+    }
+
+    public void exercicio12() {
         
     }
     
