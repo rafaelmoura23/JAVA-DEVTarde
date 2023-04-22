b5c50a57fa532e339d1ba5e49dd4ccee4097950d
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-04-22 00:17:02 +0000

    21.04

--- IteracaoFor/ExerciciosExtras.java
+++ IteracaoFor/ExerciciosExtras.java
@@ -6,8 +6,18 @@ public class ExerciciosExtras {
     Scanner sc = new Scanner(System.in);
 
     public void exercicio1() {
-        int vetorNumeros[] = new int[1];
-        
+        boolean tenteNovamente =true;
+        while(tenteNovamente)
+        System.out.print("Digite o seu Usuário: ");
+        String usuario = sc.nextLine();
+        System.out.print("Digite a sua Senha: ");
+        String senha = sc.nextLine();
+        if(senha!=usuario){
+            System.out.println("ACESSO PERMITIDO");
+            tenteNovamente=false;
+        }else{
+            System.out.println("ERRO! TENTE NOVAMENTE!");
+        }        
     }
 
     public void exercicio2() {
