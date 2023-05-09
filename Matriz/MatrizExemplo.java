e40103137d95f235c92f2e6255843b800661d7a6
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-09 17:29:04 +0000

    09.05

--- Matriz/MatrizExemplo.java
+++ Matriz/MatrizExemplo.java
@@ -13,7 +13,7 @@ public class MatrizExemplo {
         for(int i = 0; i < 10; i++ ){ // 1ª dimensão
             for (int j = 0; j < 9; j++) { // 2ª dimensão
                 //System.out.print("matriz["+i+"]["+j+"]=");
-                matriz[i][j] = rd.nextInt(9); //usando random
+                matriz[i][j] = rd.nextInt(10); //usando random
             }
         }
         //impressão dos elementos da matriz de forma indvidual
@@ -21,7 +21,14 @@ public class MatrizExemplo {
             for (int j = 0; j < matriz[0].length; j++) { // 2ª dimensão
                 System.out.println("matriz["+i+"] ["+j+"]=" +matriz[i][j]);
             }
-            
+        }
+        //imprimir em formato de matriz 10x9
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] +" ");                
+            }
+            System.out.println("|");
         }
     }
 }
