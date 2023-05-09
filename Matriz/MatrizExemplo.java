638f7268b3a588ea3c3673bb7e54ef33cb849c17
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-09 17:02:00 +0000

    09.05

---
+++ Matriz/MatrizExemplo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MatrizExemplo {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+
+    public void exemplo1(){
+        //criar preencher e manipular a matriz
+        //declarar a matriz
+        int matriz[][]= new int[10][9]; //vetor de duas dimensões
+        //preencher minha matriz
+        for(int i = 0; i < 10; i++ ){ // 1ª dimensão
+            for (int j = 0; j < 9; j++) { // 2ª dimensão
+                System.out.println("matriz[]");
+                
+            }
+        }
+    }
+}
