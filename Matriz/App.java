bc00ff7426098367e30f7b02f9a172b07ad24d72
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-14 13:44:28 +0000

    14.05

--- Matriz/App.java
+++ Matriz/App.java
@@ -1,7 +1,7 @@
 public class App{
     public static void main(String[] arg){
         MatrizExercicios obj = new MatrizExercicios();
-        obj. exercicio2();
+        obj. exercicio3();
 
         
 }
--- Matriz/MatrizExercicios.java
+++ Matriz/MatrizExercicios.java
@@ -39,7 +39,7 @@ public class MatrizExercicios {
             }
             System.out.println("|");
         }
-        // Encontrando o maior valor e sua localização
+        // localizar o maior valor
         int maiorValor = matriz[0][0];
         int linha = 0;
         int coluna = 0;
@@ -52,19 +52,56 @@ public class MatrizExercicios {
                 }
             }
         }
-         System.out.println("O maior valor encontrado é " + maiorValor + " e está na linha " + (linha+1) + " e na coluna " + (coluna+1) + ".");
+             System.out.println("O maior valor encontrado é " + maiorValor + " e está na linha " + (linha+1) + " e na coluna " + (coluna+1) + ".");
     }
-
 
 
     public void exercicio3() {
         int matriz[][]= new int[5][5];
+        int valorX;
         for(int i = 0; i < matriz.length; i++ ){ // 1ª dimensão
             for (int j = 0; j < matriz.length; j++) { // 2ª dimensão
-                matriz[i][j] = rd.nextInt(10); 
+                matriz[i][j] = rd.nextInt(10,99); 
             }
         }
+         //printar a matriz 
+         for (int i = 0; i < matriz.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] +" ");                
+            }
+            System.out.println("|");
+        }
+        System.out.print("Informe um valor: (10 a 99)"); // valor X a ser encontrado na matriz
+        valorX = sc.nextInt();
+
+        boolean encontrado = false;
+        int linha = 0, coluna = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == valorX) {
+                    encontrado = true;
+                    linha = i;
+                    coluna = j;
+                    break;
+                }
+            }
+            if (encontrado) {
+                break;
+            }
+        }
+        // Imprime a localização do valor ou mensagem de "não encontrado"
+        if (encontrado) {
+            System.out.print("O valor " +valorX+  " foi encontrado na linha " +linha+  " e na coluna " +coluna);
+        } else {
+            System.out.print("O valor " +valorX+  " não foi encontrado na matriz.");
+        }
     }
+
+
+
+
+
 
     public void exercicio4() {
     }
