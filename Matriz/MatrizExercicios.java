67a9cd7f4edb3243842c859a6cc22fbcfccf1a02
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-14 19:42:14 +0000

    14.05

--- Matriz/MatrizExercicios.java
+++ Matriz/MatrizExercicios.java
@@ -143,16 +143,16 @@ public class MatrizExercicios {
                 B[i][j] = sc.nextInt();
             }
         }
-
+        // multiplicar a matriz A por B
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
+                    C[i][j] = A[i][j] * B[i][j];
                 }
             }
         }
 
-        // Imprimindo a matriz  C
+        // Printar a matriz  C
         System.out.println("A * B = C:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
