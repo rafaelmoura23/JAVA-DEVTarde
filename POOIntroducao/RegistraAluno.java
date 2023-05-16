9f346a77668eacc9b99e3b5ee779145fb4366ebb
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-16 17:07:23 +0000

    16.05

---
+++ POOIntroducao/RegistraAluno.java
@@ -0,0 +1,11 @@
+package POOIntroducao;
+
+public class RegistraAluno {
+        //declaração dos atributos do objeto
+        private String nome;
+        private String endereco;
+        private int idade;
+        private double notaMatematica;
+        private double notaPortugues;
+        private double notaCiencias;
+}
