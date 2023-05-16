cbace2f0612b68c98fd3708a7b5d7b9a8ac800fb
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-16 17:51:06 +0000

    16.05

--- POOIntroducao/App.java
+++ POOIntroducao/App.java
@@ -2,6 +2,51 @@ package POOIntroducao;
 
 public class App {
     public static void main(String[] args) {
-        
+        // criando objetos
+        RegistraAluno aluno1 = new RegistraAluno(); // criei um objeto
+        RegistraAluno aluno2 = new RegistraAluno(); // criei um objeto
+        RegistraAluno aluno3 = new RegistraAluno(); // criei um objeto
+
+        // setando os atibutos (SET)
+
+        aluno1.setNome("Ana Carla Pereira");
+        aluno1.setEndereco("Rua Carlos Gomes");
+        aluno1.setIdade(19);
+        aluno1.setNotaCiencias(10);
+        aluno1.setNotaMatematica(10);
+        aluno1.setNotaPortugues(9);
+
+        aluno2.setNome("João Paulo");
+        aluno2.setEndereco("Rua pedro Elias");
+        aluno2.setIdade(15);
+        aluno2.setNotaCiencias(5);
+        aluno2.setNotaMatematica(7);
+        aluno2.setNotaPortugues(8);
+
+        aluno3.setNome("Roberto Carlos");
+        aluno3.setEndereco("Rua Carlos Zaccaria");
+        aluno3.setIdade(20);
+        aluno3.setNotaCiencias(10);
+        aluno3.setNotaMatematica(6);
+        aluno3.setNotaPortugues(3);
+
+        // mostrando os atributos com o (GET)
+        System.out.println("Nome: " + aluno1.getNome());
+        System.out.println("Endereço: " + aluno1.getEndereco());
+        System.out.println("Idade: " + aluno1.getIdade());
+        System.out.println("Media: " + aluno1.getMedia());
+
+        System.out.println("Nome: " + aluno2.getNome());
+        System.out.println("Endereço: " + aluno2.getEndereco());
+        System.out.println("Idade: " + aluno2.getIdade());
+        System.out.println("Media: " + aluno2.getMedia());
+
+        System.out.println("Nome: " + aluno3.getNome());
+        System.out.println("Endereço: " + aluno3.getEndereco());
+        System.out.println("Idade: " + aluno3.getIdade());
+        System.out.println("Media: " + aluno3.getMedia());
+
+        System.out.println("Contador: " + RegistraAluno.getQuantidadeAlunos());
+
     }
 }
--- POOIntroducao/RegistraAluno.java
+++ POOIntroducao/RegistraAluno.java
@@ -1,11 +1,79 @@
 package POOIntroducao;
 
 public class RegistraAluno {
-        //declaração dos atributos do objeto
-        private String nome;
-        private String endereco;
-        private int idade;
-        private double notaMatematica;
-        private double notaPortugues;
-        private double notaCiencias;
+    // declaração dos atributos do objeto
+    private String nome;
+    private String endereco;
+    private int idade;
+    private double notaMatematica;
+    private double notaPortugues;
+    private double notaCiencias;
+    // atributo da classe
+    private static int contadorEstudante;
+
+
+    // metodos(ações)
+    // criação de metodos do tipo get (acesso) 
+    // retorna o nome do estudante
+    public String getNome() {
+        return nome;
+    }
+
+    // retorna o endereço do estudante
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // retorna idade do estudante
+    public int getIdade() {
+        return idade;
+    }
+
+    // retorna a média do estudante
+    public double getMedia() {
+        double resultado = 0;
+        resultado = (notaMatematica
+                + notaPortugues
+                + notaCiencias) / 3;
+        return resultado;
+    }
+
+    // retorna a quantidade de estudantes cadastrados
+    public static int getQuantidadeAlunos() {
+        return contadorEstudante;
+    }
+
+
+
+    // metodos de definição (set)
+
+    // define ou altera o nome do estudante
+    public void setNome(String temp) {
+        nome = temp;
+    }
+
+    // define ou altera o endereço do estudante
+    public void setEndereco(String temp) {
+        endereco = temp;
+    }
+
+    // define ou altera o endereço do estudante
+    public void setIdade(int temp) {
+        idade = temp;
+    }
+
+    // define ou altera o endereço do estudante
+    public void setNotaMatematica(double temp) {
+        notaMatematica = temp;
+    }
+
+    // define ou altera o endereço do estudante
+    public void setNotaPortugues(double temp) {
+        notaPortugues = temp;
+    }
+
+    // define ou altera o endereço do estudante
+    public void setNotaCiencias(double temp) {
+        notaCiencias = temp;
+    }
 }
