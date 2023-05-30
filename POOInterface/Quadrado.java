f1ca1c39d3a00a1285b20bae3bdadad4f7bbf175
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-30 17:00:10 +0000

    30

--- POOInterface/Quadrado.java
+++ POOInterface/Quadrado.java
@@ -7,19 +7,16 @@ public class Quadrado implements FigurasGeometricas{
 
     @Override
     public double getArea() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public String getNomeFigura() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public double getPerimetro() {
-        // TODO Auto-generated method stub
         return 0;
     } 
     
