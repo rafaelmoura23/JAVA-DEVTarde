dfa26f7a7bc0641581af2825d84d13489f182d55
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-30 18:45:23 +0000

    30.05

--- ExercicioPOO/App.java
+++ ExercicioPOO/App.java
@@ -2,6 +2,18 @@ package ExercicioPOO;
 
 public class App {
     public static void main(String[] args) {
-        
+        Pessoa people1 = new Pessoa();  //criar um objeto
+
+        // setando os atibutos (SET)
+        people1.setNome("Ana Carla Pereira");
+        people1.setAltura(1.80);
+        people1.setDia(23);
+        people1.setMes(10);
+        people1.setAno(2003);
+
+        // mostrando os atributos com o (GET)
+        System.out.println("Nome: " + people1.getNome());
+        System.out.println("Altura: " + people1.getAltura());
+        System.out.println("Data de Nascimento: " + people1.getDia() + "/" + people1.getMes()+ "/"+people1.getAno());
     }
 }
--- ExercicioPOO/Pessoa.java
+++ ExercicioPOO/Pessoa.java
@@ -3,7 +3,44 @@ package ExercicioPOO;
 public class Pessoa {
      // declaração dos atributos do objeto
      private String nome;
-     private String dataNascimento;
+     private int dia;
+     private int mes;
+     private int ano;
      private double altura;
+
+    // metodos de definição (set)
+    // define ou altera o nome do estudante
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getDia() {
+        return dia;
+    }
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    public int getMes() {
+        return mes;
+    }
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+     
     
 }
