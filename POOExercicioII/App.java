745359ad61001c67feb5e2d288239282605517d7
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-30 19:46:35 +0000

    30.05

--- POOExercicioII/App.java
+++ POOExercicioII/App.java
@@ -1,4 +1,19 @@
 package POOExercicioII;
 
+import javax.swing.JOptionPane;
+
 public class App {
+    public static void main(String[] args) {
+        //vetor de objetos
+        Agenda obj[]=new Agenda[10];  //vetor de objetos que cabem 10 objetos
+
+        for (int i = 0; i < obj.length; i++) {
+            obj[i] = new Agenda(); //construindo o objeto 
+            //preencher o objeto
+            obj[i].setNome(JOptionPane.showInputDialog("Nome: "));
+            obj[i].setIdade(JOptionPane.showInputDialog("Idade: "));
+            obj[i].setAltura(JOptionPane.showInputDialog("Altura: "));
+            
+        }
+}
 }
