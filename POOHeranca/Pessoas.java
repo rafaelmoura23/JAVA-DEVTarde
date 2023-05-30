6114d4a8adfd51eb318ea50c58a2bf180d6ccebd
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-30 16:48:08 +0000

    30.05

--- POOHeranca/Pessoas.java
+++ POOHeranca/Pessoas.java
@@ -1,39 +1,45 @@
 package POOHeranca;
 
-public  abstract class Pessoas {
-    //superclasse - fornece herança para as outras classes
-    //atributos genéricos - comuns a todas as classes herdeiras
-    //abstract - é uma classe que não pode ser instanciada como objeto
+public abstract class Pessoas {
+    // superclasse - fornece herança para as outras classes
+    // atributos genéricos - comuns a todas as classes herdeiras
+    // abstract - é uma classe que não pode ser instanciada como objeto
 
-    //ATRIBUTOS 
+    // ATRIBUTOS
     String nome;
     String endereco;
     int idade;
     String cpf;
-    String rg;   //superclasse
 
-    //MÉTODOS
+    // MÉTODOS
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getEndereco() {
         return endereco;
     }
+
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
