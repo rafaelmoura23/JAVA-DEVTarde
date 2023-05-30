c66d3b397c2500908870728b6436f1c1f8899533
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-05-30 19:30:55 +0000

    30.05

--- ExercicioPOO/App.java
+++ ExercicioPOO/App.java
@@ -1,22 +1,10 @@
 package ExercicioPOO;
 
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) {
         Pessoa people1 = new Pessoa();  //criar um objeto
-
-        // setando os atibutos (SET)
-        people1.setNome("Ana Carla Pereira");
-        people1.setAltura(1.80);
-        people1.setDia(23);
-        people1.setMes(10);
-        people1.setAno(2003);
-        people1.setIdade(2023-2003);
-
-        // mostrando os atributos com o (GET)
-        System.out.println("Nome: " + people1.getNome());
-        System.out.println("Altura: " + people1.getAltura());
-        System.out.println("Data de Nascimento: " + people1.getDia() + "/" + people1.getMes()+ "/"+people1.getAno());
-        System.out.println("Idade: " +people1.getIdade());
-
-    }
+        people1.setNome(JOptionPane.showInputDialog("Digite sua Idade: "));
+}
 }
--- ExercicioPOO/Pessoa.java
+++ ExercicioPOO/Pessoa.java
@@ -1,51 +1,71 @@
 package ExercicioPOO;
 
 public class Pessoa {
-     // declaração dos atributos do objeto
-     private String nome;
-     private int dia;
-     private int mes;
-     private int ano;
-     private double altura;
-     private int idade;
+    // declaração dos atributos do objeto (atributos)
+    private String nome;
+    private int dia;
+    private int mes;
+    private int ano;
+    private double altura;
 
-    // metodos de definição (set)
-    // define ou altera o nome do estudante
+    // construtor
+
+    // sets and gests (métodos)
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getDia() {
         return dia;
     }
+
     public void setDia(int dia) {
         this.dia = dia;
     }
+
     public int getMes() {
         return mes;
     }
+
     public void setMes(int mes) {
         this.mes = mes;
     }
+
     public int getAno() {
         return ano;
     }
+
     public void setAno(int ano) {
         this.ano = ano;
     }
+
     public double getAltura() {
         return altura;
     }
+
     public void setAltura(double altura) {
         this.altura = altura;
     }
-    public int getIdade() {
-        return idade-ano;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
+
+    // printar todas as informações
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Altura: " + altura);
+        System.out.println("Data de Nascimento: " + dia + "/" + mes + "/" + ano);
     }
 
+    // calcula a idade da pessoa
+    public void calculoIdade() {
+        int idade = 0;
+        if (dia >= 30 && mes <= 5) {
+            idade = 2023 - ano;
+        } else {
+            idade = 2023 - ano - 1;
+            System.out.println("A idade é: " + idade);
+        }
+    }
 }
