c459effae78cca3fb007804a4383049026e87950
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-04 18:03:53 +0000

    04.06

--- POOElevador/App.java
+++ POOElevador/App.java
@@ -1,13 +1,22 @@
 package POOElevador;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //criar um objeto
         Elevador pessoas1 = new Elevador(0, 10, 5, 0);
         //setar os objetos
-        pessoas1.entrar();
-        pessoas1.subir();
-        pessoas1.descer();
-        pessoas1.sair();
+
+        System.out.println("Porta do Elevador Aberta");
+
+        System.out.print("Digite a Capacidade do Elevador");
+        int capacidadePessoas = scanner.nextInt();
+
+        System.out.print("Informe o número de andares: ");
+        int totalAndares = scanner.nextInt();
+
+
   }
 }
