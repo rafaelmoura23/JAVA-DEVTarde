a08505f351a8b728175ba635538ba826dfcdea40
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-04 18:47:59 +0000

    04.06

--- POOElevador/App.java
+++ POOElevador/App.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         Elevador pessoas1 = new Elevador(); //criar um objeto
 
-        System.out.print("Digite a Capacidade de Pessoas no Elevador: ");
+        // pedir a capacidade para o usuario e guardar na variavel capacidade
+        System.out.print("Digite a Capacidade de Pessoas no Elevador: "); 
         int capacidadePessoas = scanner.nextInt();
 
+        // pedir o numero de andares para o usuario e guardar na andares
         System.out.print("Informe o número de andares: ");
         int totalAndares = scanner.nextInt();
 
+        // atribuindo os valores digitados pelo usuario para o construtor
         pessoas1.Inicializar(capacidadePessoas, totalAndares); 
 
-
+        // iniciar o metodo para ficar rodando o programa até que o usuario saia
         System.out.println("Porta do Elevador Aberta");
-        boolean Continue = true;
-
+        boolean Continue = true; //quando boolean for false então programa finaliza   [switch?]
         while(Continue){
             System.out.println("Escolha uma opção: [1]-Entrar, [2]-Subir, [3]-Descer, [4]-Sair, [5]-Finish");
             int opcao = scanner.nextInt();
-            
-
+        
             if(opcao==1){
                 pessoas1.entrar();
             } else if(opcao==2){
@@ -35,7 +37,7 @@ public class App {
             } else if(opcao==5){
                 Continue = false;
             } else{
-                System.out.println("Escolha uma opção de 1 a 5.");
+                System.out.println("Escolha uma opção de 1 a 5."); //se o usuario digitar algum numero fora do range então...
             }
         }
   }
