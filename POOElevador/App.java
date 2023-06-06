d0a818de010a04ca921a8ed1ad2716c851ca5f2a
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-06-06 15:06:21 -0300

    06.06

--- POOElevador/App.java
+++ POOElevador/App.java
@@ -17,8 +17,28 @@ public class App {
             System.out.println("Digite a ação desejada:" +"\n1-Entrar"+"\n2-Sair"+"\n3-Subir"+"\n4-Descer"+"\n5-Desligar");
             int acao = sc.nextInt();
             
+            switch (acao) {
+                case 1:
+                    System.out.println("Capacidade Atual: "+elevador1.entra());
+                    break;
+                case 2:
+                    System.out.println("Capacidade Atual: "+elevador1.sai());
+                    break;
+                case 3:
+                    System.out.println("Quantos Andares deseja Subir? ");
+                    System.out.println("Andar Atual: "+elevador1.subir(sc.nextInt()));
+                    break;
+                case 4:
+                    System.out.println("Quantos Andares deseja Descer? ");
+                    System.out.println("Andar Atual: "+elevador1.descer(sc.nextInt()));
+                    break;
+                case 5:
+                    ligado=false;
+                    break;
+                default:
+                System.out.println("Digite uma ação válida.");
+                    break;
+            }
         }
-
-     
 }
 }
