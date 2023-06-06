b0b93c79e402cec46f17ff1516dbb044cf298d92
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-06 16:40:49 +0000

    06.06

--- POOExercicioII/App.java
+++ POOExercicioII/App.java
@@ -1,17 +1,23 @@
 package POOExercicioII;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
-        //vetor de objetos
-        Agenda obj[]=new Agenda[10];  //vetor de objetos que cabem 10 objetos
+        Random rd = new Random();
+        //criar um vetor para 10 objetos
+        Agenda contatos[]=new Agenda[10];  //vetor de objetos que cabem 10 objetos
 
-        for (int i = 0; i < obj.length; i++) {
-            obj[i] = new Agenda(); //construindo o objeto 
-            //preencher o objeto
-            obj[i].setNome("Pessoa"+i);
-            obj[i].setIdade("Idade"+i);
-            obj[i].setAltura("Altura"+i);
+        //criar os objetos e preencher os atributos do objeto
+        for (int i = 0; i < contatos.length; i++) {
+            //criar o objeto - construtor
+            contatos[i] = new Agenda(); //construtor vazio
+            //preencher os atributos (sets)
+            contatos[i].setNome(JOptionPane.showInputDialog("Digite o Nome: "));
+            contatos[i].setIdade(i+18);
+            contatos[i].setAltura(rd.nextDouble());
         }
         System.out.println();
 
