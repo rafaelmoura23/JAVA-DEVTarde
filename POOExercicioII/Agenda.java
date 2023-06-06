ec574fb68ade1e5b1d20584c5bd090dbcba495ff
rafaelmoura23 <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-06 16:31:34 +0000

    06.06

--- POOExercicioII/Agenda.java
+++ POOExercicioII/Agenda.java
@@ -1,30 +1,54 @@
 package POOExercicioII;
 
+import javax.swing.JOptionPane;
+
 public class Agenda {
     //atributos
     String nome;
-    String altura;
-    String idade;
+    int idade;
+    double altura;
+    
 
     //métodos
+    //construtor
+    public Agenda(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    //metodos do tipo return e void
+    //construtor vazio 
+    public Agenda() {
+    }
+    //getters and setter
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getAltura() {
-        return altura;
-    }
-    public void setAltura(String altura) {
-        this.altura = altura;
-    }
-    public String getIdade() {
+    public int getIdade() {
         return idade;
     }
-    public void setIdade(String idade) {
+    public void setIdade(int idade) {
         this.idade = idade;
     }
-
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
     
+    //metodos de aplicação diversas
+    // imprimir(void)
+    public void imprimir() {
+        JOptionPane.showMessageDialog(null, "Nome: "+nome +"\nAltura: " + altura +"\nIdade: " + idade );
+        
+    }
+    
+    
+
+
+
 }
