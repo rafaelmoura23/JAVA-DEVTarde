b400bfe1152060add408d3c1289cabacd0de3b27
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-10 16:56:20 +0000

    10.06

--- ProjectPOO/App.java
+++ ProjectPOO/App.java
@@ -1,8 +1,5 @@
 package ProjectPOO;
 
-import java.util.Scanner;
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) {
 
--- ProjectPOO/Pessoa.java
+++ ProjectPOO/Pessoa.java
@@ -1,6 +1,61 @@
 package ProjectPOO;
 
-import java.util.Random;
-
 public class Pessoa { 
+    //atributos
+    String nome;
+    double saldo;
+    int nDaConta;
+
+    //metodos
+    //construtor
+    public Pessoa(String nome, double saldo, int nDaConta) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.nDaConta = nDaConta;
+    }
+
+     //getter and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getnDaConta() {
+        return nDaConta;
+    }
+
+    public void setnDaConta(int nDaConta) {
+        this.nDaConta = nDaConta;
+    }
+
+    //outros metodos (sacar, depositar e saldo?), metodos do tipo void(vazio)
+    public void deposito(double valor) { //metodo deposito, valor = valor depositado
+        saldo+=valor; //o valor depositado será somado ao saldo atual
+        System.out.println("Você depositou " +valor+ " Reais.");
+    }
+
+    public void saque(double valor) { // metodo saque, valor = valor que o usuario ira sacar
+        if(valor>=saldo){
+            System.out.println("Saldo Insuficiente para Sacar");
+        } else{
+            
+        }
+        
+    }
+    
+    
+
+
+
     }
