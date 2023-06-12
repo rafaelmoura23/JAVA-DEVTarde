b20e4e23b2094de0199b258e85693df2e368ff67
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-12 00:55:25 +0000

    11.06

--- ProjectPOO/PessoaFisica.java
+++ ProjectPOO/PessoaFisica.java
@@ -1,5 +1,22 @@
 package ProjectPOO;
 
-public class PessoaFisica extends Pessoa{
+public class PessoaFisica extends Pessoa{  //classe pessoFisica tem herança com a superclasse pessoa, ou seja, herda os atributos nome, ndaconta e saldo
+
+    String cpf;
+
+    public PessoaFisica(String nome, double saldo, int nDaConta) { //super-construtor ?????
+        super(nome, saldo, nDaConta);
+    }
+
+    //gett and sett de cpf
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
 
 }
--- ProjectPOO/PessoaJuridica.java
+++ ProjectPOO/PessoaJuridica.java
@@ -1,6 +1,22 @@
 package ProjectPOO;
 
-public class PessoaJuridica extends Pessoa{
-    
+public class PessoaJuridica extends Pessoa{ //classe pessojusridica tem herança com a superclasse pessoa, ou seja, herda os atributos nome, ndaconta e saldo
+
+    String cnpj;
+
+    //construtor
+    public PessoaJuridica(String nome, double saldo, int nDaConta) { 
+        super(nome, saldo, nDaConta);
+    }  
+
+    //gett and sett de cnpj
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    } 
     
 }
