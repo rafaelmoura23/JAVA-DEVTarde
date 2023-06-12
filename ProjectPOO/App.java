9da1bf06c856ee98353983fafa8abd7cedead743
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-12 01:18:09 +0000

    11.06

--- ProjectPOO/App.java
+++ ProjectPOO/App.java
@@ -1,7 +1,26 @@
 package ProjectPOO;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //scanner
+        CriacaoContas criar = new CriacaoContas(); //criação de um objeto
+        boolean logado = true; // declarar um boolean para enquanto for true continuar rodando o app
+
+        System.out.println("Seja Bem vindo ao App do Banco!");
+
+        while(logado){
+            System.out.println("Para continuar escolha uma opção: \n 1- Cadastro de Pessoas; \n 2- Busca de Contas; \n 3- Sair");
+            int opcaoEscolhida = sc.nextInt(); 
+            System.out.println("");
+
+            if(opcaoEscolhida == 1){
+                
+
+            }
+        }
+
 
     }
 }
--- ProjectPOO/CriacaoContas.java
+++ ProjectPOO/CriacaoContas.java
@@ -2,7 +2,18 @@ package ProjectPOO;
 
 import java.util.Scanner;
 
+import ExercicioPOO.Pessoa;
+
 public class CriacaoContas {
-   
-       
+    Pessoa[] vPessoas = new Pessoa[100]; //criar um vetor de pessoas, inicalmente com 100 pessoas
+    int contPessoas = 0;  // contador para o número de pessoas, ou seja o numero de cadastros realizados, max=100
+
+
+
+
+    public void cadastroFisico() {
+        
+        
+    }
+     
 }
