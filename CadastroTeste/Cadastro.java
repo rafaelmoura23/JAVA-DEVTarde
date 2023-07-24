f69b44a40092e3071d8423f70db80678b310062d
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-07-24 19:30:24 +0000

    24.07

---
+++ CadastroTeste/Cadastro.java
@@ -0,0 +1,44 @@
+package CadastroTeste;
+
+public class Cadastro{
+
+    String nome;
+    String nomeEvento;
+    String dataEvento;
+    int nParticipantes;
+
+    //getts and setts
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getnomeEvento() {
+        return nomeEvento;
+    }
+    public void setnomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public String getdataEvento() {
+        return dataEvento;
+    }
+    public void setdataEvento(String dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public int nParticipantes() {
+        return nParticipantes;
+    }
+    public void setnParticipantes(int nParticipantes) {
+        this.nParticipantes = nParticipantes;
+    }
+
+    
+    
+
+
+    
+}
