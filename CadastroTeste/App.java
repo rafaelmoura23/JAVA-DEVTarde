8ce7028310dc124bcfa05deef993b864fb2a33e6
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-07-24 19:36:29 +0000

    24.07

--- CadastroTeste/App.java
+++ CadastroTeste/App.java
@@ -6,5 +6,12 @@ public class App {
     Cadastro evento1[] = new Cadastro[10];
     boolean cadastrar = true; 
     int cont=0;
+
+    while(cadastrar){
+        int acao = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma ação:"
+                                                        +"\n 1-Cadastrar Evento;"
+                                                        +"\n 2-Consultar Evento;"
+                                                        +"\n 3-Sair."));
+    }
 }
 }
