e87b3521b8edc7d32a1184be270bd055a31bafbd
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-03 14:32:08 -0300

    03.08

--- Exception/ExemploTryCatch.java
+++ Exception/ExemploTryCatch.java
@@ -7,17 +7,35 @@ public class ExemploTryCatch {
 
         //tratando as exceptions
         boolean calcular = true;
+        int n1=0,n2=0;
         while(calcular){
         try{
         //recebendo os dados
-        int n1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um Número Inteiro"));
-        int n2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um outro Número Inteiro"));
-
+        boolean n1ok = true;
+        while(n1ok){
+            try{
+            n1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um Número Inteiro"));
+            n1ok = false;
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
+        }    
+        }
+        
+        boolean n2ok = true;
+        while(n2ok){
+            try{
+                n2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um outro Número Inteiro"));
+                n2ok=false;
+            } catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
+            }
+        }
         //mostrando o resultado
         JOptionPane.showMessageDialog(null, "O Resultado é: " 
                                                                     +(n1+n2));
                                                                     calcular = false;
                                                                     
+         calcular = false;                                                           
         }catch(Exception erro1){
             JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
             }
