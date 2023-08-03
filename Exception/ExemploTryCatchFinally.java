ef899b63067bff440ded82d03d5561af5a3e56fe
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-03 16:04:30 -0300

    03.08

--- Exception/ExemploTryCatchFinally.java
+++ Exception/ExemploTryCatchFinally.java
@@ -1,8 +1,25 @@
 package Exception;
 
+import javax.swing.JOptionPane;
+
 public class ExemploTryCatchFinally {
     public static void main(String[] args) {
         String frase = null;
         String fraseUpper = null;
+        frase = JOptionPane.showInputDialog("Digite uma frase");
+        
+        if(frase.equals("")){
+            frase=null;
+        }
+
+        try{
+            fraseUpper = frase.toUpperCase();
+        } catch(NullPointerException erro){
+            JOptionPane.showMessageDialog(null, "Erro - String Vazia");
+            frase = "A Frase estava Vazia";
+        } finally{
+            fraseUpper = frase.toUpperCase();
+            JOptionPane.showMessageDialog(null, fraseUpper);
     }
+}
 }
