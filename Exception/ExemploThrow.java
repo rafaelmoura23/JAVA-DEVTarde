f0ceb78bf9adbb1fb4c214ebc17091478eb3b0c8
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-03 16:58:42 -0300

    03.08.2023

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -5,21 +5,23 @@ import javax.swing.JOptionPane;
 public class ExemploThrow {
     public static void main(String[] args) {
         // 
-        try{
-        JOptionPane.showMessageDialog(null, "Welcome to SENAI");
-        String login = JOptionPane.showInputDialog("Informe seu login: ");
-        String senha = JOptionPane.showInputDialog("Informe sua senha de 6 digítos: ");
-        String dataNasc = JOptionPane.showInputDialog("Informe sua data de nascimento: ");   
-
-        if(senha.length()!=6){
-            throw new Exception("senha deve conter 6 digítos");
-        } else if(senha.equals(login)){
-            throw new Exception("senha não pode ser igual ao login");
-        } else if(senha.equals(dataNasc)){
-            throw new Exception("senha não pode se igual a data de nascimento");
+       try {
+            JOptionPane.showMessageDialog(null, "Welcome to SENAI");
+            String login = JOptionPane.showInputDialog("Informe seu Login");
+            String dataNascimento = JOptionPane.showInputDialog("Informe sua Data de Nascimento [dd/mm/aa]");
+            String senha = JOptionPane.showInputDialog("Informe sua Senha de 6 Digitos");
+            dataNascimento.replace("/", "");
+            if (senha.length() != 6) {
+                throw new Exception("Senha deve ter 6 Digitos");
+            }else if(senha.equals(login)){
+                throw new Exception("Senha igual Login");
+            } else if(senha.equals(dataNascimento)){
+                throw new Exception("Senha igual Data de Nascimento");
+            }
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
-    } catch(Exception e){
-        JOptionPane.showMessageDialog(null, e.getMessage());
-    }
     }
 }
