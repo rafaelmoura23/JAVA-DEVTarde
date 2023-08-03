ae119982d6244ccc438bb0391a48dc05638270e3
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-03 15:13:31 -0300

    03.08

--- Exception/ExemploTryCatch.java
+++ Exception/ExemploTryCatch.java
@@ -5,50 +5,64 @@ import javax.swing.JOptionPane;
 public class ExemploTryCatch {
     public static void main(String[] args) {
 
-        //tratando as exceptions
+        // tratando as exceptions
         boolean calcular = true;
-        int n1=0,n2=0;
-        while(calcular){
-            int calculadora = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma Operação \n 1-Adição \n 2-Subtração \n 3-Multiplicação \n 4-Divisão"));
-            if(calculadora==1){
-                try{
-        //recebendo os dados
-        boolean n1ok = true;
-        while(n1ok){
-            try{
-            n1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um Número Inteiro"));
-            n1ok = false;
-        } catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
-        }    
-        }
-        
-        boolean n2ok = true;
-        while(n2ok){
-            try{
-                n2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um outro Número Inteiro"));
-                n2ok=false;
-            } catch(Exception e){
-                JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
+        int n1 = 0, n2 = 0;
+
+        while (calcular) {
+                try {
+                    // recebendo os dados
+                    boolean n1ok = true;
+                    while (n1ok) {
+                        try {
+                            n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Nº inteiro"));
+                            n1ok = false;
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
+                        }
+
+                    }
+                    boolean n2ok = true;
+                    while (n2ok) {
+                        try {
+                            n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro Nº inteiro"));
+                            n2ok = false;
+                        } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null, "Erro - valor digitado n é inteiro");
+                        }
+                    }
+                    // mostrando o resultado
+                    //escolher o tipo de conta
+
+                    String operacao[] = {"Adição","Subtração","Multiplicação","Divisão","Sair"};
+                    int acao = JOptionPane.showOptionDialog(null, "Escolha a Operação", "Calculadora", JOptionPane.DEFAULT_OPTION, JOptionPane.CANCEL_OPTION, null, operacao, operacao[0]);
+                    if(acao==0){
+                        JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 + n2));
+                    } else if(acao==1){
+                        JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 - n2));
+                    } else if(acao==2){
+                        JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 * n2));
+                    } else if(acao==3){
+                        JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 * n2));
+                    } else{
+                        calcular=false;
+                    }
+
+
+
+
+
+
+                    
+                    calcular = false;
+                } catch (Exception erro1) {
+                    JOptionPane.showMessageDialog(null, "Erro - valor digitadno não é inteiro6");
+                }
             }
         }
-        //mostrando o resultado
-        JOptionPane.showMessageDialog(null, "O Resultado é: " 
-                                                                    +(n1+n2));
-                                                                    calcular = false;
-                                                                    
-         calcular = false;                                                           
-        }catch(Exception erro1){
-            JOptionPane.showMessageDialog(null, "Erro - valor digitado não é inteiro");
-            }
-        } else if(calculadora==2){
+    }
 
-        } else if(calculadora==3){
-
-        } else if(calculadora==4){
-            
-        }
-    } 
-            }
-
-}
