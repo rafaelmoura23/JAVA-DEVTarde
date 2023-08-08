1ec53e57d161679e1a7415bcc2031b66ec343af2
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-08 16:42:55 -0300

    Throw

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class ExemploThrow {
     public static void main(String[] args) {
-        // 
+        // criação de exceptions
        try {
             JOptionPane.showMessageDialog(null, "Welcome to SENAI");
             String login = JOptionPane.showInputDialog("Informe seu Login");
@@ -12,16 +12,16 @@ public class ExemploThrow {
             String senha = JOptionPane.showInputDialog("Informe sua Senha de 6 Digitos");
             dataNascimento.replace("/", "");
             if (senha.length() != 6) {
-                throw new Exception("Senha deve ter 6 Digitos");
+                throw new Exception("Senha deve ter 6 Digitos");   //criação de exceptions
             }else if(senha.equals(login)){
                 throw new Exception("Senha igual Login");
             } else if(senha.equals(dataNascimento)){
                 throw new Exception("Senha igual Data de Nascimento");
+            } else if(!senha.matches(".*[a-z]+.*") || !senha.matches(".*[A-Z]+.*") ){
+                throw new Exception("falta letra minuscula ou maiuscula");
             }
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-            JOptionPane.showMessageDialog(null, e.getMessage());
+            } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "senha", 0);
         }
     }
 }
---
+++ testes/App.java
@@ -0,0 +1,9 @@
+package testes;
+
+public class App {
+    public static void main(String[] args) {
+
+        boolean palavra = "rafael".matches(".*a.*");
+		System.out.println(palavra);
+}
+}
