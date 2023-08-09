728d0a79d47fad2ce64c74a72a43ec6d7ed79335
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-09 15:53:20 -0300

    LinkedList

--- Colecoes/App.java
+++ Colecoes/App.java
@@ -3,8 +3,9 @@ package Colecoes;
 public class App {
     public static void main(String[] args) {    
         ExemploArrayList obj1 = new ExemploArrayList();
-        obj1.teste();
-        ExemploArrayList obj2 = new ExemploArrayList();
-        obj2.exercicio1();
+        //obj1.teste();
+        //obj1.exercicio1();
+        ExemploLinkedList obj2 = new ExemploLinkedList();
+        obj2.teste();
     }
 }
--- Colecoes/ExemploLinkedList.java
+++ Colecoes/ExemploLinkedList.java
@@ -6,7 +6,18 @@ public class ExemploLinkedList {
     LinkedList <String> pessoas = new LinkedList<>();
 
     public void teste(){
-
+        //adiciona normalmente
+        pessoas.add("Pedro");
+        pessoas.add("Maria");
+        pessoas.add("João");
+        System.out.println(pessoas + " - lista normal");
+        //adiciona na primeira posição
+        pessoas.addFirst("Cecília");
+        System.out.println(pessoas + " - adicionado cecilia na primeira posição");
+        //remove o último nome
+        pessoas.removeLast();
+        //imprimi a lista
+        System.out.println(pessoas + " - removido o ultimo nome (joão)");
     }
     
 }
