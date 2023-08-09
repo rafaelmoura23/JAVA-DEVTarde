e94a349e30b8a3ba2cbef9b74064f8bb49a6387f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-09 15:19:43 -0300

    09.08

--- Colecoes/ExemploLinkedList.java
+++ Colecoes/ExemploLinkedList.java
@@ -3,7 +3,7 @@ package Colecoes;
 import java.util.LinkedList;
 
 public class ExemploLinkedList {
-    LinkedList <String> pessoas = new LinkedList();
+    LinkedList <String> pessoas = new LinkedList<>();
 
     public void teste(){
 
