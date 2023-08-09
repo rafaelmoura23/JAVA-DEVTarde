8e02ba1f7f5fff961e9ece382656387b023c2ad3
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-09 14:11:13 -0300

    09.08

---
+++ Colecoes/App.java
@@ -0,0 +1,9 @@
+package Colecoes;
+
+public class App {
+    public static void main(String[] args) {
+        ExemploArrayList obj1 = new ExemploArrayList();
+        obj1.teste();
+    }
+    
+}
---
+++ Colecoes/ExemploArrayList.java
@@ -0,0 +1,16 @@
+package Colecoes;
+
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+    public void teste(){
+        ArrayList <String> carros = new ArrayList<>();
+        carros.add("Fusca");
+        carros.add("Brasília");
+        carros.add("Chevette");
+        carros.add("Monza");
+        carros.add("Monza");
+        System.out.println(carros);
+    }
+    
+}
