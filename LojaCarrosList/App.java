a58e46ea3d51f6f035038ac1c2f43a9ece403982
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-10 15:04:00 -0300

    10.08

--- LojaCarrosList/App.java
+++ LojaCarrosList/App.java
@@ -15,8 +15,8 @@ public class App {
         //apagar
 
         boolean lojaAberta = true;
+        JOptionPane.showMessageDialog(null, "Bem vindo(a) a loja de Carros!");
         while(lojaAberta){
-            JOptionPane.showMessageDialog(null, "Bem vindo(a) a loja de Carros!");
             int opcao = Integer.parseInt(JOptionPane.showInputDialog("Opções: \n 1-Cadastrar Carro \n 2-Listar Carros \n 3-Consultar Carro \n 4-Apagar Carro \n 5-Sair"));
 
             if(opcao==1){
@@ -26,23 +26,27 @@ public class App {
                 c1.setModelo(JOptionPane.showInputDialog("Informe o modelo do carro:"));
                 c1.setCor(JOptionPane.showInputDialog("Informe a cor do carro:"));
                 c1.setAno(JOptionPane.showInputDialog("Informe o ano do carro:"));
-
-                listaCarros.add(c1); //adicionado o carro no arrayList
+                listaCarros.add(c1); //adicionado os carros no arrayList
 
             } else if(opcao==2){
                 for (Carros carros : listaCarros) {
-                    listar+=carros.getMarca() + " | "+carros.getModelo() + " | " + carros.getCor() + " | " + carros.getAno() + "\n";
+                    listar+=carros.getMarca() + " | "+carros.getModelo() + " | " + carros.getCor() + " | " + carros.getAno();
                     JOptionPane.showMessageDialog(null, listar);
-                    listar="";
+                    listar = "";
                 }
             } else if(opcao==3){
+                String consultar = JOptionPane.showInputDialog("Informe o cadastro que deseja consultar");
+                if(consultar.equals(listar)){
+                    listaCarros.clear();
+                }
 
             } else if(opcao==4){
+                JOptionPane.showInputDialog("Informe o cadastro que deseja excluir");
 
             } else if(opcao==5){
-
-            } else{
                 lojaAberta = false;
+            } else{
+                JOptionPane.showMessageDialog(null, "Informe uma opção válida");
             }
 
         }
