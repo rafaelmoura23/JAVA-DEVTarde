9e0649b45ac9cd57efe167279d0b4091049c68bf
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-10 16:57:50 -0300

    10.08

--- AWTPrimeiroFrame/src/ExemploFrame.java
+++ AWTPrimeiroFrame/src/ExemploFrame.java
@@ -1,4 +1,8 @@
 import java.awt.Frame;
 public class ExemploFrame extends Frame {
-    
+    public ExemploFrame(){
+    super();
+    this.setSize(300,500);
+    this.setVisible(true);
+    }
 }
---
+++ AWTPrimeiroFrame/src/ExemploJFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class ExemploJFrame extends JFrame{
+    public ExemploJFrame(){
+        super();
+        this.setSize(500,300);
+        this.setVisible(true);
+
+    }
+}
