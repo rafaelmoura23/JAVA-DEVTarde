f6fdf1dc069f5fe453cc6959dfb71ef298b49b5f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-31 15:19:22 -0300

    GUIFrames

---
+++ GUIContainerComponents/src/ContainerComponent.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+
+public class ContainerComponent extends JFrame {
+    public ContainerComponent() {
+        super("Janela Pricipal");
+    }
+}
---
+++ GUIContainerComponents/src/JFrameComponents.java
@@ -0,0 +1,21 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+
+public class JFrameComponents extends JFrame {
+    public JFrameComponents() {
+        super("Janela Principal");
+        this.setBounds(500, 250, 500, 500);
+        //modicando o layout do frame (por padrão vem GridLayout)
+        FlowLayout flow = new FlowLayout(); //  usado para mudar o layout, usando apenas o setBounds os botões criados no fori ficam sobrepostos no tamanho do frame inteiro
+        this.setLayout(flow); // usando o FlowLayout os botões ficam lado a lado
+        //adicionar os componentes
+        for (int i = 1; i <=10; i++) {
+            this.add(new JButton(""+i)); //gerando 10 botões
+        }
+        //set frame
+        this.setDefaultCloseOperation(2);
+        this.setVisible(true);
+    }
+    
+}
