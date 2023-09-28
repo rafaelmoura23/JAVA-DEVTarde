3be8eba3acfce5895c343b9110dc4b748a3f9b00
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-09-28 16:35:15 -0300

    exercicio5Cardlayout

--- GuiTabbedCards/src/App.java
+++ GuiTabbedCards/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         //new ExemploCardLayout();
         //new Exercicio1CardLayout();
-        new Exercicio3CardLayout();
-
+        //new Exercicio3CardLayout();
+        new Exercicio5CardLayout();
     }
 }
--- GuiTabbedCards/src/Exercicio5CardLayout.java
+++ GuiTabbedCards/src/Exercicio5CardLayout.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Exercicio5CardLayout  extends JFrame{
-    
+public class Exercicio5CardLayout extends JFrame{
+    public Exercicio5CardLayout() {
+        super("Exercício 5");
+
+        
+    }
 }
