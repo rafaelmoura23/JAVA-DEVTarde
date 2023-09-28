a87eb591d994a17e85e59e998b32e96a3faf3b25
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-09-28 16:58:10 -0300

    card5layout

--- GuiTabbedCards/src/Exercicio5CardLayout.java
+++ GuiTabbedCards/src/Exercicio5CardLayout.java
@@ -1,10 +1,28 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.CardLayout;
 
-public class Exercicio5CardLayout extends JFrame{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Exercicio5CardLayout extends JFrame {
     public Exercicio5CardLayout() {
-        super("Exercício 5");
+        super("Exercicio 5- Quiz");
+        //Criar um Painel Principal -> cardLayout
+        CardLayout cl = new CardLayout();
+        JPanel mainCard = new JPanel(cl);// set do layout
+        this.add(mainCard);
+        //criar os Cards
+        // card1 - Tela Inicial
+        JPanel card1 = new JPanel();
+        card1.add(new JLabel("Jogo Quiz - Seja bem Vindo"));
+        JButton but1 = new JButton("Start");//botão da card1
+        mainCard.add(card1,"Start");
+        //card2 - Primeira Pergunta
+        JPanel card2 = new JPanel();
+        card2.add(new JLabel("Quem foi Campeão da Copa de 70"));
 
-        
+
+
     }
 }
