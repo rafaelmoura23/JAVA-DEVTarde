057cefd38ad78c4d2a148693872066f1a77b90aa
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-10-31 16:53:53 -0300

    JDBC-DAO

--- JDBCConnectionTest/src/App.java
+++ JDBCConnectionTest/src/App.java
@@ -3,7 +3,7 @@ import Connection.ConnectionDAO;
 public class App {
     public static void main(String[] args) throws Exception {
         new ConnectionDAO().criaTabela();
-        new ConnectionDAO().inserir("Rafael de Sousa", "rafael@gmail.com");
-        new ConnectionDAO().buscarPorId(1);
+        new ConnectionDAO().inserir("Rafael", "rafael123@gmail.com");
+        new ConnectionDAO().buscarPorId(4);
     }
 }
